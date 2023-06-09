1e46352deb218bbc70e25a3aeb88f79984ce6233
Snyong <62795814+spencer0124@users.noreply.github.com>
2023-06-09 23:03:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('language-select').addEventListener('change', function(e) {
-        var language = e.target.value;
-        if (language === 'en') {
-            document.getElementById('user-id').placeholder = 'Enter your ID';
-            document.getElementById('user-pw').placeholder = 'Enter your Password';
-            document.getElementById('submit-button').innerText = 'Submit';
-        } else {
-            document.getElementById('user-id').placeholder = '킹고ID를 입력하세요';
-            document.getElementById('user-pw').placeholder = '킹고 비밀번호를 입력하세요';
-            document.getElementById('submit-button').innerText = '로그인';
-        }
-    });
-
     document.getElementById('login-form').addEventListener('submit', function(e) {
         e.preventDefault();
 
@@ -25,53 +12,49 @@ document.addEventListener('DOMContentLoaded', function() {
             "rememberMe": true
         };
 
-        // Show loading message
-        document.getElementById('loading').style.display = 'block';
+        
 
         fetch('https://lib.skku.edu/pyxis-api/api/login', {
             method: 'POST',
             headers: {
                 "Host": "lib.skku.edu",
                 "Content-Type": "application/json;charset=utf-8",
+                "Origin": "file://",
                 "Connection": "keep-alive",
                 "Accept": "application/json, text/plain, */*",
-                
+                "Accept-Language": "ko",
+                "Accept-Encoding": "gzip, deflate, br",
             },
             body: JSON.stringify(payload)
         })
         .then(response => {
             if (!response.ok) {
-                throw new Error('Network response was not ok');
+                throw new Error('네트워크가 불안정합니다');
             }
             return response.json();
         })
         .then(data => {
-            // Hide loading message
-            document.getElementById('loading').style.display = 'none';
-            
-            
             
 
             if (data.data && data.data.name && data.data.dept && data.data.branchGroup && data.data.memberNo && data.data.patronState) {
                 document.getElementById('response').innerHTML = 
-                `Name: ${data.data.name}<br>
-                Dept: ${data.data.dept.name}<br>
-                parentDept: ${data.data.parentDept.name}<br>
-                patronType: ${data.data.patronType.name}<br>
-                Branch Group Name: ${data.data.branchGroup.name}<br>
-                MemberNo: ${data.data.memberNo}<br>
-                Patron State: ${data.data.patronState.name}<br>
-                <strong>Status: Login Success</strong><br>
+                `[이름]: ${data.data.name}<br>
+                [학부]: ${data.data.dept.name}<br>
+                [소속]: ${data.data.parentDept.name}<br>
+                [유형]: ${data.data.patronType.name}<br>
+                [캠퍼스]: ${data.data.branchGroup.name}<br>
+                [학번]: ${data.data.memberNo}<br>
+                [재학 여부]: ${data.data.patronState.name}<br>
                 <img src="https://idcard.skku.edu/idt/photo.do?userId=${data.data.memberNo}" alt="User Photo"/>`;
             } else {
-                document.getElementById('response').innerHTML = '<strong>Status: Data fields missing in response</strong>';
+                document.getElementById('response').innerHTML = '<strong>로그인에 실패하였습니다</strong>';
             }
         })
         .catch(error => {
             console.error('Error:', error);
             // Hide loading message
-            document.getElementById('loading').style.display = 'none';
-            document.getElementById('response').innerHTML = '<strong>Status: Login Failed</strong>';
+           
+            document.getElementById('response').innerHTML = '<strong>예기치 못한 오류가 발생했습니다</strong>';
         });
     });
 });
